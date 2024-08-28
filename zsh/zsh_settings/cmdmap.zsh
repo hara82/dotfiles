d46b5c0c15352f9f391a0c8e1f3d5dd0a4fdc1cc
Ryosuke Hara <hara-ryosuke573@g.ecc.u-tokyo.ac.jp>
2024-08-29 01:37:13 +0900

    [zsh] switch from abbr to abbrev-alias

--- zsh/zsh_settings/cmdmap.zsh
+++ zsh/zsh_settings/cmdmap.zsh
@@ -1,6 +1,9 @@
 alias cat="bat --style=plain"
 alias ls="eza"
 alias tree="eza -T"
+alias abbr="abbrev-alias"
 
-abbr -S add doc="docker container" >> /dev/null
-abbr -S up="cd .." >> /dev/null
+abbr up="cd .."
+abbr d="docker"
+abbr -r dc="d compose"
+abbr gc="git commit"
