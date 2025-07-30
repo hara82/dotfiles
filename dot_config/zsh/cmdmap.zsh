b618b4b43bb90080dfbca22c240ecb7898dc9fe9
Ryosuke Hara <92095956+hara82@users.noreply.github.com>
2025-07-30 23:38:22 +0900

    change zsh settings location

---
+++ dot_config/zsh/cmdmap.zsh
@@ -0,0 +1,13 @@
+alias cat="bat --style=plain"
+alias ls="eza"
+alias tree="eza -T"
+alias abbr="abbrev-alias"
+alias imgcat="wezterm imgcat"
+
+abbr up="cd .."
+abbr d="docker"
+abbr -r dcp="d compose"
+abbr -r dc="d container"
+abbr gc="git commit"
+abbr gs="git status"
+abbr ggl="google"
